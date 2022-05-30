aef7785bdedf64191bb698f07c295d313fdd3431
max397574 <max.heinzer397@gmx.ch>
2022-05-30 16:33:35 +0200

    feat: some first values and settings

--- lua/colortils/init.lua
+++ lua/colortils/init.lua
@@ -1,3 +1,22 @@
 local colortils = {}
 
+local settings = {
+    register = "+",
+    ---String: "block"|"hex"
+    color_display = "block",
+}
+
+local utils = require("colortils.utils")
+
+local red = 0
+local green = 0
+local blue = 0
+local buf = nil
+local win = nil
+local ns = vim.api.nvim_create_namespace("ColorPicker")
+
+colortils.setup = function(update)
+    settings = vim.tbl_deep_extend("force", settings, update or {})
+end
+
 return colortils
