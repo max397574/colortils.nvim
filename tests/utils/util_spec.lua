86f35e4a7ca2751625f95ca6323bc64475142da1
max397574 <max.heinzer397@gmx.ch>
2022-05-30 16:33:53 +0200

    test(utils): added tests for bars and blocks

--- tests/utils/util_spec.lua
+++ tests/utils/util_spec.lua
@@ -7,3 +7,27 @@ describe("Get hex values", function()
         assert.equals(require("colortils.utils").hex(12), "0C")
     end)
 end)
+
+describe("Get blocks", function()
+    it("Get empty partial block", function()
+        assert.equals(require("colortils.utils").get_partial_block(0.124), "")
+    end)
+    it("Get any partial block", function()
+        assert.equals(
+            require("colortils.utils").get_partial_block(0.598),
+            "▌"
+        )
+    end)
+    it("Get bar without partial block", function()
+        assert.equals(
+            require("colortils.utils").get_bar(30, 90, 9),
+            "███"
+        )
+    end)
+    it("Get bar with partial block", function()
+        assert.equals(
+            require("colortils.utils").get_bar(30, 93, 11),
+            "███▌"
+        )
+    end)
+end)
